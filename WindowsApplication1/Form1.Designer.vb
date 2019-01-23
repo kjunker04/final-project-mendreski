411790cb39e3cf91fdbb9051b69182a731e28e7f
chris junker <37341069+cjunks94@users.noreply.github.com>
2019-01-22 22:18:31 -0500

    Add project files.

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,196 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnProcess = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.txtCSZ = New System.Windows.Forms.TextBox()
+        Me.txtChairs = New System.Windows.Forms.TextBox()
+        Me.txtSofa = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnProcess
+        '
+        Me.btnProcess.Location = New System.Drawing.Point(167, 672)
+        Me.btnProcess.Name = "btnProcess"
+        Me.btnProcess.Size = New System.Drawing.Size(175, 27)
+        Me.btnProcess.TabIndex = 0
+        Me.btnProcess.Text = "Process Order"
+        Me.btnProcess.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(691, 672)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(143, 22)
+        Me.btnClear.TabIndex = 1
+        Me.btnClear.Text = "Clear Order Form"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(144, 95)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Customer name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(144, 108)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "(Last, First)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(179, 184)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Address:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(148, 255)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(79, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "City, State, Zip:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(98, 328)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(129, 13)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Number of chairs ordered:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(101, 441)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(126, 13)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "Number of sofas ordered:"
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.Location = New System.Drawing.Point(702, 62)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(384, 433)
+        Me.lstOutput.TabIndex = 8
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(291, 105)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 20)
+        Me.txtName.TabIndex = 9
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Location = New System.Drawing.Point(291, 184)
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(100, 20)
+        Me.txtAddress.TabIndex = 10
+        '
+        'txtCSZ
+        '
+        Me.txtCSZ.Location = New System.Drawing.Point(291, 252)
+        Me.txtCSZ.Name = "txtCSZ"
+        Me.txtCSZ.Size = New System.Drawing.Size(100, 20)
+        Me.txtCSZ.TabIndex = 11
+        '
+        'txtChairs
+        '
+        Me.txtChairs.Location = New System.Drawing.Point(291, 321)
+        Me.txtChairs.Name = "txtChairs"
+        Me.txtChairs.Size = New System.Drawing.Size(100, 20)
+        Me.txtChairs.TabIndex = 12
+        '
+        'txtSofa
+        '
+        Me.txtSofa.Location = New System.Drawing.Point(291, 434)
+        Me.txtSofa.Name = "txtSofa"
+        Me.txtSofa.Size = New System.Drawing.Size(100, 20)
+        Me.txtSofa.TabIndex = 13
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1156, 762)
+        Me.Controls.Add(Me.txtSofa)
+        Me.Controls.Add(Me.txtChairs)
+        Me.Controls.Add(Me.txtCSZ)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnProcess)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnProcess As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lstOutput As ListBox
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents txtCSZ As TextBox
+    Friend WithEvents txtChairs As TextBox
+    Friend WithEvents txtSofa As TextBox
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+    Private Sub btnProcess_Click(sender As Object, e As EventArgs) Handles btnProcess.Click
+        Dim sr As IO.StreamReader = IO.File.OpenText("C:\Users\junk\Documents\Visual Studio 2015\Projects\WindowsApplication1\PRICE&TAXDATA.txt")
+        Dim name, addr, invoiceNum, firstName, lastName, CSZ As String
+        Dim numChairs, numSofa, cost, tax, totalCost, costChair, costSofa, taxRate, zip As Double
+
+        getfileInfo(sr, costChair, costSofa, taxRate)
+        getuserInfo(name, addr, CSZ, numChairs, numSofa)
+        splitName(name, firstName, lastName)
+        splitCSZ(CSZ, zip)
+    End Sub
+
+    Sub getfileInfo(ByVal sr As IO.StreamReader, ByRef costChair As Double, ByRef costSofa As Double, ByRef taxRate As Double)
+        costChair = CDbl(sr.ReadLine)
+        costSofa = CDbl(sr.ReadLine)
+        taxRate = CDbl(sr.ReadLine)
+    End Sub
+
+    Sub getuserInfo(ByRef name As String, ByRef addr As String, ByRef CSZ As String, ByRef numChair As Double, ByRef numSofa As Double)
+        name = txtName.Text
+        addr = txtAddress.Text
+        CSZ = txtCSZ.Text
+        numChair = CDbl(txtChairs.Text) 'chris said to check for bad user input maybe
+        numSofa = CDbl(txtSofa.Text)    'same here. casting "" to double is no good
+    End Sub
+
+    Sub splitName(ByVal name As String, ByRef firstName As String, ByRef lastName As String)
+        Dim words As String() = Split(name, ",")
+        firstName = words(1).Trim
+        lastName = words(0).Trim
+        lstOutput.Items.Add(firstName & " " & lastName)
+    End Sub
+    Sub splitCSZ(ByVal CSZ, ByRef zip)
+        Dim words As String() = Split(CSZ, " ")
+        words(2).Trim.Substring(1, 4)
+        zip = CDbl(words(2).Trim.Substring(1, 4))
+        lstOutput.Items.Add(zip)
+    End Sub
+End Class
